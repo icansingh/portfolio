13205310b338c6f54861b02f1faf0f8cee27900f
icansingh <ikjyot@ualberta.ca>
2025-06-10 18:21:57 -0600

    Project Setup

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { BrowserRouter } from "react-router-dom"
+
+
+unction App() {
+
+
+  return (
+    <>
+      <BrowserRouter>
+      
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
